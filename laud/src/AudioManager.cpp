4e31dc0cbd876d0c9357fdc5edc84d7e78cdb16f
JIEEEN <tjtpp009@korea.ac.kr>
2025-08-30 14:07:43 +0900

    fix: typo

--- laud/src/AudioManager.cpp
+++ laud/src/AudioManager.cpp
@@ -36,60 +36,60 @@ void AudioManager::init() const {
 }
 
 const std::vector<int16_t> &AudioManager::recordAudio(const int record_time) {
-  int totalFrames = this->sample_rate * record_time;
-  std::vector<int16_t> recordedData(totalFrames);
+  int total_frames = this->sample_rate * record_time;
+  std::vector<int16_t> recorded_data(total_frames);
 
-  PaStream *inputStream;
+  PaStream *input_stream;
   PaError err = Pa_OpenDefaultStream(
-      &inputStream, static_cast<int>(this->input_channel_type), 0, paInt16,
+      &input_stream, static_cast<int>(this->input_channel_type), 0, paInt16,
       this->sample_rate, this->frames_per_buffer, nullptr, nullptr);
 
   if (err != paNoError) {
     LOG_ERROR("Open input stream error: ", Pa_GetErrorText(err));
-    return recordedData;
+    return recorded_data;
   }
 
-  err = Pa_StartStream(inputStream);
+  err = Pa_StartStream(input_stream);
   if (err != paNoError) {
     LOG_ERROR("Start input stream error: ", Pa_GetErrorText(err));
-    return recordedData;
+    return recorded_data;
   }
 
   LOG_INFO("Recording ", record_time, " seconds...");
 
-  int framesRecorded = 0;
-  while (framesRecorded < totalFrames) {
-    int framesToRead =
-        std::min(this->frames_per_buffer, totalFrames - framesRecorded);
-    err = Pa_ReadStream(inputStream, recordedData.data() + framesRecorded,
-                        framesToRead);
+  int frames_recorded = 0;
+  while (frames_recorded < total_frames) {
+    int frames_to_read =
+        std::min(this->frames_per_buffer, total_frames - frames_recorded);
+    err = Pa_ReadStream(input_stream, recorded_data.data() + frames_recorded,
+                        frames_to_read);
     if (err && err != paInputOverflowed) {
       LOG_ERROR("Read error: ", Pa_GetErrorText(err));
       break;
     }
-    framesRecorded += framesToRead;
+    frames_recorded += frames_to_read;
   }
 
-  Pa_StopStream(inputStream);
-  Pa_CloseStream(inputStream);
+  Pa_StopStream(input_stream);
+  Pa_CloseStream(input_stream);
 
   LOG_INFO("Recording finished!");
 
-  return recordedData;
+  return recorded_data;
 }
 
 void AudioManager::playRecordedAudio(const std::vector<int16_t> &recorded_data,
                                      const int record_time) {
-  PaStream *outputStream;
+  PaStream *output_stream;
   PaError err = Pa_OpenDefaultStream(
-      &outputStream, 0, static_cast<int>(this->output_channel_type), paInt16,
+      &output_stream, 0, static_cast<int>(this->output_channel_type), paInt16,
       this->sample_rate, this->frames_per_buffer, nullptr, nullptr);
   if (err != paNoError) {
     LOG_ERROR("Open output stream error: ", Pa_GetErrorText(err));
     return;
   }
 
-  err = Pa_StartStream(outputStream);
+  err = Pa_StartStream(output_stream);
   if (err != paNoError) {
     LOG_ERROR("Start output stream error: ", Pa_GetErrorText(err));
     return;
@@ -98,22 +98,22 @@ void AudioManager::playRecordedAudio(const std::vector<int16_t> &recorded_data,
   LOG_INFO("Playing back...");
 
   const int total_frames = this->sample_rate * record_time;
-  int framesPlayed = 0;
-  while (framesPlayed < total_frames) {
-    int framesToWrite =
-        std::min(this->frames_per_buffer, total_frames - framesPlayed);
-    err = Pa_WriteStream(outputStream, recorded_data.data() + framesPlayed,
-                         framesToWrite);
+  int frames_played = 0;
+  while (frames_played < total_frames) {
+    int frames_to_write =
+        std::min(this->frames_per_buffer, total_frames - frames_played);
+    err = Pa_WriteStream(output_stream, recorded_data.data() + frames_played,
+                         frames_to_write);
 
     if (err && err != paOutputUnderflowed) {
       LOG_ERROR("Write Error: ", Pa_GetErrorText(err));
       break;
     }
-    framesPlayed += framesToWrite;
+    frames_played += frames_to_write;
   }
 
-  Pa_StopStream(outputStream);
-  Pa_CloseStream(outputStream);
+  Pa_StopStream(output_stream);
+  Pa_CloseStream(output_stream);
 
   return;
 }
